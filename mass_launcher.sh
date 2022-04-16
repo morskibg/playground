b3c4e62cdc1cf072daf8ba16930dcf8cbf13a869
morskibg <dimityrp@yahoo.com>
2022-04-16 15:01:12 +0000

    logger modified

--- mass_launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/dpetkov/playground/venv/bin/activate
-for i in {1..50}
-do
-   python3 psycopg2_thread_bench.py 1000
-   python3 psycopg_sync_bench.py 5000
-   python3 asyncpg_bench.py 5000 single_query
-   
-done
